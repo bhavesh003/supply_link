3d21fa78afeb0a884ec5c0f1169aaaa6ed0e7410
bhavesh003 <hiraybhavesh03@gmail.com>
2026-04-11 14:12:44 +0000

    day4

--- server/src/main/java/com/edutech/progressive/entity/Supplier.java
+++ server/src/main/java/com/edutech/progressive/entity/Supplier.java
@@ -5,7 +5,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class Supplier implements Comparable<Supplier> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int supplierId;
     private String supplierName;
     private String email;
