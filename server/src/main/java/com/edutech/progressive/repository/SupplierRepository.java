ac27dd37b0cb392b9a72ff22ffe7521d8daa8ab7
bhavesh003 <hiraybhavesh03@gmail.com>
2026-04-16 15:32:24 +0000

    day12

--- server/src/main/java/com/edutech/progressive/repository/SupplierRepository.java
+++ server/src/main/java/com/edutech/progressive/repository/SupplierRepository.java
@@ -1,11 +1,6 @@
 package com.edutech.progressive.repository;
 
-import java.util.Optional;
-
-import javax.transaction.Transactional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -13,18 +8,13 @@ import org.springframework.stereotype.Repository;
 import com.edutech.progressive.entity.Supplier;
 
 @Repository
-public interface SupplierRepository extends JpaRepository<Supplier, Integer>{
-    @Modifying
-    @Transactional
-    @Query("delete from Supplier s where s.supplierId = :supplierId")
+public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
+
     void deleteBySupplierId(@Param("supplierId") int supplierId);
-    
-    @Query("select s from Supplier s where s.supplierId = :supplierId")
+
     Supplier findBySupplierId(@Param("supplierId") int supplierId);
-    
-    @Query("select s from Supplier s where s.supplierName = :supplierName")
-    Supplier findByUsername(@Param("supplierName") String username);
-     
-    @Query("select s from Supplier s where s.email = :email")
-    Supplier findByEmail(@Param("email") String email);
+
+    Supplier findByUsername(String username);
+
+    Supplier findByEmail(String email);
 }
