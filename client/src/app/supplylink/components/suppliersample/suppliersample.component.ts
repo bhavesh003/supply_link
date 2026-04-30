61f2ac8eccae03830e40fc44e1f00d1808ca3fb5
bhavesh003 <hiraybhavesh03@gmail.com>
2026-04-30 16:19:31 +0000

    day20

--- client/src/app/supplylink/components/suppliersample/suppliersample.component.ts
+++ client/src/app/supplylink/components/suppliersample/suppliersample.component.ts
@@ -9,11 +9,11 @@ import { of } from 'rxjs';
   imports: [], // Add necessary imports
   templateUrl: './suppliersample.component.html',
   styleUrls: ['./suppliersample.component.css'],
-  template:'Supplier Details :<br>Supplier ID: {{supplier.supplierId}}<br>Name: {{supplier.supplierName}}<br>Email: {{supplier.email}}<br>Phone: {{supplier.phone}}<br>Address: {{supplier.address}}<br>Username: {{supplier.username}}<br>Password: {{supplier.password}}<br>Role: {{supplier.role}}',
-  
+  template: 'Supplier Details :<br>Supplier ID: {{supplier.supplierId}}<br>Name: {{supplier.supplierName}}<br>Email: {{supplier.email}}<br>Phone: {{supplier.phone}}<br>Address: {{supplier.address}}<br>Username: {{supplier.username}}<br>Password: {{supplier.password}}<br>Role: {{supplier.role}}',
+
 })
 export class SupplierSampleComponent {
   // Component logic goes here
-  supplier = new Supplier(1,"John Wane","johnwane@gmail.com","9876543210","texas","johnwane","July@101","USER");
-  
+  supplier = new Supplier(1, "John Wane", "johnwane@gmail.com", "9876543210", "texas", "johnwane", "July@101", "USER");
+
 }
--- client/src/app/supplylink/components/warehousesample/warehousesample.component.ts
+++ client/src/app/supplylink/components/warehousesample/warehousesample.component.ts
@@ -10,6 +10,6 @@ import { Warehouse } from '../../types/Warehouse';
   template: 'Warehouse ID: {{warehouse.warehouseId}}<br>Supplier ID: {{warehouse.supplierId}}<br>Warehouse Name: ${{warehouse.warehouseName}}<br>Location: ${{warehouse.location}}<br>Capacity: ${{warehouse.capacity}}'
 })
 export class WarehouseSampleComponent {
-  warehouse = new Warehouse(1,"2","Flamingo","Nevada",1000);
+  warehouse = new Warehouse(1, "2", "Flamingo", "Nevada", 1000);
 
 }
